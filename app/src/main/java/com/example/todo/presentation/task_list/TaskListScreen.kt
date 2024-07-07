8d01ab3ce257f797fe4418fc09cb4463ac1da58a
NicolasOrsag <nicolas.orsag@gmail.com>
2024-07-07 15:17:04 +0200

    Improve UI of TaskListScreen

--- app/src/main/java/com/example/todo/presentation/task_list/TaskListScreen.kt
+++ app/src/main/java/com/example/todo/presentation/task_list/TaskListScreen.kt
@@ -1,20 +1,93 @@
 package com.example.todo.presentation.task_list
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.todo.data.local.model.Task
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun TaskListScreen(viewModel: TaskListViewModel = koinViewModel()) {
     val tasks by viewModel.tasks.collectAsState()
-    
-    LazyColumn {
-        items(tasks){
-            Text(text = it.title)
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(tasks) { task ->
+                TaskItem(task = task)
+            }
+        }
+
+        FloatingActionButton(
+            onClick = { }, // Implement this function in your ViewModel
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp),
+            containerColor = MaterialTheme.colorScheme.secondary
+        ) {
+            Text("+")
+        }
+    }
+}
+
+@Composable
+fun TaskItem(task: Task) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = task.title,
+                style = MaterialTheme.typography.labelMedium,
+                modifier = Modifier.weight(4f)
+            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier.weight(1f)
+            ) {
+                Checkbox(checked = task.completed, onCheckedChange = {})
+                IconButton(onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Default.Delete, contentDescription = "Delete Task"
+                    )
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/todo/presentation/theme/Theme.kt
+++ app/src/main/java/com/example/todo/presentation/theme/Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
@@ -18,9 +19,11 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color(0xFF32bc9b),
+    secondary = Color(0xFFff784b),
+    tertiary = Pink40,
+    surface = Color(0xFFf2f2f2),
+    background = Color(0xFFf2f2f2)
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -36,15 +39,9 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun ToDoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
